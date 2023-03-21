82cdca9a71e72a0d3543fe39ab1f70669dfafbf2
Karthick <56195227+Karthick3290@users.noreply.github.com>
2023-03-21 14:53:15 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,4 +1,16 @@
-def function(){
-  echo 'This is run by a groovy script..'
+def buildApp(){
+  echo 'This is run by a groovy script for building the application..'
+}
+return this
+
+
+def testApp(){
+  echo 'This is run by a groovy script for testing the application..'
+}
+return this
+
+def deployApp(){
+  echo 'This is run by a groovy script for deploying the application..'
+  echo "The deployed version is ${params.VERSION}"
 }
 return this
